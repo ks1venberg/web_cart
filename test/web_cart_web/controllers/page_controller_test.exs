1e366d1f27afc4196f6218ca50a5b2e6ccdf2f19
Konstantin Grebnev <venberg.dev@gmail.com>
2024-07-10 19:00:34 +0200

    updated heex files

--- test/web_cart_web/controllers/page_controller_test.exs
+++ test/web_cart_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule WebCartWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+    assert html_response(conn, 200) =~ "WebCart application"
   end
 end
